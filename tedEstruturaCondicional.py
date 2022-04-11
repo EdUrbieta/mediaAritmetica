c277770f8b9b0655d939f64fd3da87a9fc3c5ce8
BonbuBot <92870959+BonbuBot@users.noreply.github.com>
2022-04-11 12:30:45 -0300

    Update tedEstruturaCondicional.py

--- tedEstruturaCondicional.py
+++ tedEstruturaCondicional.py
@@ -7,9 +7,9 @@
 #No caso do aluno em recuperação e prova final, mostre também quanto o aluno irá precisar para passar.
 #No caso da recuperação a nota necessária para passar é dada por 10 – Média + 2 e na prova final é dado por 10 – Média.
 
-nota1 = float(input("Digite a primeira nota:"))
-nota2 = float(input("Digite a segunda nota:"))
-nota3 = float(input("Digite a terceira nota:"))
+nota1 = float(input("Digite a primeira nota: "))
+nota2 = float(input("Digite a segunda nota: "))
+nota3 = float(input("Digite a terceira nota: "))
 media = (nota1 + nota2 + nota3) / 3
 provaFinal = 10 - media
 recuperacao = 10 - media + 2
